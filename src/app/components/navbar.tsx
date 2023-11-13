82b8746e07e3b0f4f45d254b3375943daba59cb0
aku7703 <31360520+aku7703@users.noreply.github.com>
2023-11-13 10:51:28 -0500

    Improved styling of Navbar
    changed navbar to black; banner is a transparent png instead of an svg

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 function Navbar() {
     return (
-        <div className="navbar bg-base-100 sticky top-0 z-50">
+        <div className="navbar text-white bg-black sticky top-0 z-50">
             <div className="flex-1">
                 <Link href="/">
-                    <Image src="ph_banner.svg" alt="logo" width={300} height={128} />
+                    <Image src="/logo.png" alt="logo" width={300} height={128} />
                 </Link>
             </div>
             <div className="flex-none">
