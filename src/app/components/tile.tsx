7608597a4613d5ad77eb10da958f9dcd294af623
Madhav Kapa <madhavkapa16@gmail.com>
2024-01-28 16:09:51 -0500

    revamped projects section with modal waiting for pics

--- src/app/components/tile.tsx
+++ src/app/components/tile.tsx
@@ -24,16 +24,16 @@ function Tile({ name, title, description, imagesrc }: TileProps) {
   const isMobile = useIsMobile();
 
   return (
-    <div className={`card ${isMobile ? 'card-compact' : 'card-side'} text-white bg-gradient-to-t from-black to-gray-950 shadow-xl ${isMobile ? 'max-w-screen-sm' : 'max-w-screen-md'}`}>
-      <figure className="pl-4">
-        <Image src={imagesrc} alt={name} width={500} height={isMobile ? 100 : 100} />
-      </figure>
-      <div className="card-body max-w-screen-sm">
-        <h3 className="card-title text-red-600">{title}</h3>
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+      <div className={`card ${isMobile ? 'card-compact' : 'card-side'} text-white bg-gradient-to-t from-black to-gray-950 shadow-xl ${isMobile ? 'max-w-screen-sm' : 'max-w-screen-md'}`}>
+        <figure className="pl-4">
+          <Image src={imagesrc} alt={name} width={500} height={isMobile ? 100 : 100} />
+        </figure>
+        <div className="card-body max-w-screen-sm">
+          <h3 className="card-title text-red-600">{title}</h3>
+          <h2 className="card-title">{name}</h2>
+          <p>{description}</p>
+        </div>
       </div>
-    </div>
   );
 }
 
