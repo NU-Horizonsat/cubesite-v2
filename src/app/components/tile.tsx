6cae588e54a140cd938fc57331d76d10e89a4cbd
SpaceRage <rajiv.iyer@hotmail.com>
2024-02-01 21:57:14 -0500

    Image sizing/flexbox fixes

--- src/app/components/tile.tsx
+++ src/app/components/tile.tsx
@@ -24,16 +24,16 @@ function Tile({ name, title, description, imagesrc }: TileProps) {
   const isMobile = useIsMobile();
 
   return (
-      <div className={`card ${isMobile ? 'card-compact' : 'card-side'} text-white bg-gradient-to-t from-black to-gray-950 shadow-xl ${isMobile ? 'max-w-screen-sm' : 'max-w-screen-md'}`}>
-        <figure className="pl-4">
-          <Image src={imagesrc} className="object-cover rounded-full" alt={name} width={'500'} height={100} layout="intrinsic"/>
-        </figure>
-        <div className="card-body">
-          <h3 className="card-title text-red-600">{title}</h3>
-          <h2 className="card-title">{name}</h2>
-          <p>{description}</p>
-        </div>
+    <div className={`card flex ${isMobile ? 'card-compact' : 'card-side'} text-white bg-gradient-to-r from-black to-gray-950 shadow-xl ${isMobile ? 'max-w-screen-sm' : 'max-w-screen-md'}`}>
+      <figure className="pl-4 basis-1/4">
+        <Image src={imagesrc} className="rounded-full" alt={name} width={'500'} height={100} />
+      </figure>
+      <div className="card-body basis-3/4">
+        <h3 className="card-title text-red-600">{title}</h3>
+        <h2 className="card-title">{name}</h2>
+        <p>{description}</p>
       </div>
+    </div>
   );
 }
 
