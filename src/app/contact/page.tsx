b9cb6b2dd8317965b3362385594816a3a9089878
Madhav Kapa <madhavkapa16@gmail.com>
2024-02-13 02:43:45 +0000

    updates

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -48,7 +48,7 @@ const Contact = () => {
         </div>
       ) : (
         // name
-        <form onSubmit={handleSubmit} className="flex-col w-1/3 space-y-8 items-center">
+        <form onSubmit={handleSubmit} className="flex-col w-2/3 space-y-8 items-center">
           <div className="flex flex-col space-y-8 items-center">
             <label htmlFor="name" className="text-white">Name</label>
             <input
