5e408aa5dc19a2ec02ee173e900d89941006a693
Madhav Kapa <madhavkapa16@gmail.com>
2024-02-15 02:01:33 +0000

    error message fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react";
-import {Textarea, Input} from "@nextui-org/react"
+import { Textarea, Input } from "@nextui-org/react"
 
 
 const Contact = () => {
@@ -15,25 +15,29 @@ const Contact = () => {
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    setIsSubmitting(true);
-    try {
-      const message_comp:any = "Message from " + name + " (" + email + ")" + " - " + company +": "+ message;
-      const response = await fetch("/api/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, email, message_comp }),
-      });
-      console.log(response);
-      if (response.ok) {
-        setIsSubmitted(true);
-      } else {
-        console.error("Error sending message." + response.status + " " + response.statusText);
-        setError("Error sending message. Please try again later.");
+    if (!(name == "" && email == "" && company == "" && message == "")) {
+      setIsSubmitting(true);
+      try {
+        const message_comp: any = "Message from " + name + " (" + email + ")" + " - " + company + ": " + message;
+        const response = await fetch("/api/contact", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, email, message_comp }),
+        });
+        console.log(response);
+        if (response.ok) {
+          setIsSubmitted(true);
+        } else {
+          console.error("Error sending message." + response.status + " " + response.statusText);
+          setError("Error sending message. Please try again later.");
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
+    }else{
+      setError("Please fill in all fields!")
     }
 
     setIsSubmitting(false);
@@ -41,97 +45,97 @@ const Contact = () => {
 
   return (
 
-      <div className="flex flex-col space-y-8 items-center bg-black bg-opacity-75 m-20 rounded-2xl">
-        <p></p>
-        <p></p>
-        <h1 className="text-red-600 text-5xl text-center w-max font-bold">CONTACT US</h1>
-        {isSubmitted ? (
-          <div className="space-y-8 items-center ">
-            <h2 className="text-2xl text-white">Thank you for your message!</h2>
-            <p className="text-white">We will get back to you as soon as possible.</p>
+    <div className="flex flex-col space-y-8 items-center bg-black bg-opacity-75 m-20 rounded-2xl">
+      <p></p>
+      <p></p>
+      <h1 className="text-red-600 text-5xl text-center w-max font-bold">CONTACT US</h1>
+      {isSubmitted ? (
+        <div className="space-y-8 items-center ">
+          <h2 className="text-2xl text-white">Thank you for your message!</h2>
+          <p className="text-white">We will get back to you as soon as possible.</p>
+        </div>
+      ) : (
+        // name
+        <form onSubmit={handleSubmit} className="flex-col w-3/4 space-y-8 items-center">
+          <div className="flex flex-col space-y-8 items-center">
+            <label htmlFor="name" className="text-white">Name</label>
+            <Input
+              isRequired
+              size="md"
+              type="text"
+              name="Name"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full"
+            />
           </div>
-        ) : (
-          // name
-          <form onSubmit={handleSubmit} className="flex-col w-3/4 space-y-8 items-center">
-            <div className="flex flex-col space-y-8 items-center">
-              <label htmlFor="name" className="text-white">Name</label>
-              <Input
+          {/* email */}
+          <div className="flex flex-col space-y-8 items-center ">
+            <label htmlFor="email" className="text-white">Email</label>
+            <Input
               isRequired
               size="md"
-                type="text"
-                name="Name"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full"
-              />
-            </div>
-            {/* email */}
-            <div className="flex flex-col space-y-8 items-center ">
-              <label htmlFor="email" className="text-white">Email</label>
-              <Input
-              isRequired
+              type="email"
+              name="E-mail"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full"
+            />
+          </div>
+          {/* company */}
+          <div className="flex flex-col space-y-8 items-center ">
+            <label htmlFor="company" className="text-white">Company</label>
+            <Input
               size="md"
-                type="email"
-                name="E-mail"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full"
-              />
-            </div>
-            {/* company */}
-            <div className="flex flex-col space-y-8 items-center ">
-              <label htmlFor="company" className="text-white">Company</label>
-              <Input
-                            size="md"
 
-                type="text"
-                name="Company"
-                id="company"
-                value={company}
-                onChange={(e) => setCompany(e.target.value)}
-                className="w-full"
-              />
-            </div>
-            {/* message */}
-            <div className="flex flex-col space-y-8 items-center">
-              <label htmlFor="message" className="text-white">Message</label>
-              <Textarea
+              type="text"
+              name="Company"
+              id="company"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+              className="w-full"
+            />
+          </div>
+          {/* message */}
+          <div className="flex flex-col space-y-8 items-center">
+            <label htmlFor="message" className="text-white">Message</label>
+            <Textarea
               isRequired
               size="md"
 
-                name="Message"
-                id="message"
-                rows={10}
-                value={message}
-                maxLength={2000}
-                onChange={(e) => setMessage(e.target.value)}
-                className="w-full"
-              />
-            </div>
-            {(error === "") ? null : <div
-              className="flex flex-col space-y-8 items-center bg-red-900 border p-5 rounded-xl"
-            > 
-              <p className="text-white">{error}</p>
-            </div>}
-            <div className="flex flex-col space-y-8 items-center">
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className="text-white hover:bg-red-700 p-5 px-10 rounded-xl transition duration-300"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        )
-        }
-        <p></p>
-        <p className="text-white">Copyright 2023 - Project Horizon</p>
-        <p></p>
-      </div>
-    )
-  }
+              name="Message"
+              id="message"
+              rows={10}
+              value={message}
+              maxLength={2000}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full"
+            />
+          </div>
+          {(error === "") ? null : <div
+            className="flex flex-col space-y-8 items-center bg-red-900 border p-5 rounded-xl"
+          >
+            <p className="text-white">{error}</p>
+          </div>}
+          <div className="flex flex-col space-y-8 items-center">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="text-white hover:bg-red-700 p-5 px-10 rounded-xl transition duration-300"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )
+      }
+      <p></p>
+      <p className="text-white">Copyright 2023 - Project Horizon</p>
+      <p></p>
+    </div>
+  )
+}
 
-  export default Contact;
+export default Contact;
